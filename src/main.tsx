0911614010858ae513a5f4e44eec7010c4956813
hyeonseo1109 <alsgustj2@naver.com>
2026-08-18 23:48:47 +0900

    Feat: Main에 감싸기

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./app";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@app/providers";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
