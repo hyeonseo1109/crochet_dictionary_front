7ef326f9cc6bb44e8b9cc9e9d68268727d1e621e
hyeonseo1109 <alsgustj2@naver.com>
2026-08-19 00:47:16 +0900

    Feat: TanStack Query 커스텀훅 사용하여 데이터 가져오기 연결

--- src/widgets/ui/MainList/index.tsx
+++ src/widgets/ui/MainList/index.tsx
@@ -1,3 +1,23 @@
+import { OneList } from "@entities/ui";
+import { useStitches } from "@features/model";
+import { useEffect } from "react";
+
 export const MainList = () => {
-  return <div>MainList</div>;
+  const { data, isLoading, isError } = useStitches();
+
+  useEffect(() => {
+    console.log("data", data?.data);
+  }, [data]);
+  return (
+    <div>
+      MainList
+      {isLoading && <div>로딩 중</div>}
+      {isError && <div>에러 발생</div>}
+      <div>
+        {data?.data.map((item) => (
+          <OneList key={item.id} name={item.name} level={item.difficulty} />
+        ))}
+      </div>
+    </div>
+  );
 };
