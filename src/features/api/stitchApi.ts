074d9855872a862d52ff4e43a7f4e641e4d58846
hyeonseo1109 <alsgustj2@naver.com>
2026-08-19 00:01:38 +0900

    Feat: api호출함수 getStitches 작성

---
+++ src/features/api/stitchApi.ts
@@ -0,0 +1,14 @@
+import { ApiResponse, Stitch } from "@features/model";
+import { API_BASE_URL } from "@shared/api";
+
+export const getStitches = async (): Promise<ApiResponse<Stitch[]>> => {
+  const response = await fetch(`${API_BASE_URL}/api/stitches`);
+
+  const result: ApiResponse<Stitch[]> = await response.json();
+
+  if (!response.ok) {
+    throw new Error(result.message);
+  }
+
+  return result;
+};
